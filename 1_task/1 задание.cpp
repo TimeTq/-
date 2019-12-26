fa1f9781fc81b8aaba3a5d8dbe03b3dd3dc88415
TimeTq <vindra@inbox.ru>
2019-12-26 10:29:05 +0300

    1 и 3 задания

---
+++ 1_task/1 задание.cpp
@@ -0,0 +1,141 @@
+﻿/*ДЛЯ ВСЕХ ЗАДАЧ : файл со входными данными пользователь должен указывать сам(без хардкода).
+Задачи сдавать в виде : исходный код, примеры файлов с входными данными, и файл,
+который можно сразу запускать - уже скомпилированный,
+если язык того требует(если C#, C++ и т.д. - *.exe, если Java, Scala - *.jar и т.д.).
+Будет плюсом, если зальете решение в github.
+
+1. Считать из файла последовательность целых чисел.Вычислить 90 персентиль, 
+медиану, максимальное, минимальное и среднее значения.На вход программа получает 
+файл с числами.Каждое число в файле находится на новой строке.
+Вывод в консоль должен быть следующим :
+
+90 percentile <значение>
+median <значение>
+average <значение>
+max <значение>
+min <значение> */
+
+#include <iostream>
+#include <math.h>
+#include <fstream>
+
+
+// Быстрая сортировка
+void QuickSort(int s_arr[], int first, int last)
+{
+    if (first < last)
+    {
+        int left = first, right = last, middle = s_arr[(left + right) / 2];
+        do
+        {
+            while (s_arr[left] < middle) left++;
+            while (s_arr[right] > middle) right--;
+            if (left <= right)
+            {
+                int tmp = s_arr[left];
+                s_arr[left] = s_arr[right];
+                s_arr[right] = tmp;
+                left++;
+                right--;
+            }
+        } while (left <= right);
+        QuickSort(s_arr, first, right);
+        QuickSort(s_arr, left, last);
+    }
+}
+
+// Вычисление процентиля (определение из Вики)
+int Percentile(int arr[], int lenArr , int percent)
+{
+    int k = floor((lenArr - 1) * percent / 100 ); 
+    int result;
+
+    if (k + 1 < lenArr * percent / 100 )
+    {
+        result = arr[k + 1];
+    } else if (k + 1 == lenArr * percent / 100 ) {
+        result = (arr[k + 1] + arr[k]) / 2;
+    } else {
+        result = arr[k];
+    }
+
+    return result;
+}
+
+// Простой вывод массива
+void PrintArr(int arr[], int lenArr)
+{
+    for (int i = 0; i < lenArr; ++i)
+    {
+        std::cout << arr[i] << " | ";
+    }
+}
+
+
+double Average(int arr[], int lenArr)
+{
+    double result = 0;
+
+    for (int i = 0; i < lenArr; ++i)
+    {
+        result += arr[i];
+    }
+
+    return result/ lenArr;
+}
+
+int main()
+{
+   // Ввод пути к файлу .txt
+    std::string path= "";
+    std::cout << "Enter path: ";
+    std::cin >> path;
+  
+
+    std::ifstream file;
+    file.open(path);
+
+    if (!file.is_open()) 
+    {
+        std::cout << "no file\n" << std::endl;
+        return -1;
+    };
+
+   // Ввод значений в массив
+   int s = 0;
+   int i = 0;
+   int lenArr = 0;
+
+    // Подсчет элементов в файле
+   for (file >> s; !file.eof(); file >> s)
+   {
+       
+       lenArr += 1;
+    }
+
+
+   // Возврат в начало файла
+   file.clear();
+   file.seekg(0);
+
+   // Создание массива
+   int* a = new int[lenArr];
+   for (file >> s; !file.eof(); file >> s)
+   {
+       a[i] = s;
+       i += 1;
+   }
+
+   file.close();
+
+    QuickSort(a, 0, lenArr - 1);
+
+    // Решение
+        std::cout << "90 percentile " << Percentile(a,lenArr, 90) << std::endl;;
+        std::cout << "median " << Percentile(a, lenArr, 50) << std::endl;;
+        std::cout << "average " << Average(a, lenArr) << std::endl;
+        std::cout << "max " << a[lenArr - 1] << std::endl;;
+        std::cout << "min " << a[0];
+
+}
+
---
+++ 3_task/3 задание.cpp
@@ -0,0 +1,108 @@
+﻿/* ДЛЯ ВСЕХ ЗАДАЧ : файл со входными данными пользователь должен указывать сам(без хардкода).
+Задачи сдавать в виде : исходный код, примеры файлов с входными данными, и файл,
+который можно сразу запускать - уже скомпилированный,
+если язык того требует(если C#, C++ и т.д. - *.exe, если Java, Scala - *.jar и т.д.).
+Будет плюсом, если зальете решение в github.
+
+    3. В магазине 5 касс, в каждый момент времени к кассе стоит очередь некоторой длины.
+Каждые 30 минут измеряется средняя длина очереди в каждую кассу и для каждой кассы это значение(число вещественное)
+записывается в соответсвующий ей файл(всего 5 файлов), магазин работает 8 часов в день.
+Рассматривается только один день.
+На момент запуска приложения все значения уже находятся в файлах.Написать программу,
+которая по данным замеров определяет интервал времени,
+когда в магазине было наибольшее количество посетителей за день. */
+
+
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+
+// Простой вывод массива
+void PrintArr(double arr[], int lenArr)
+{
+    for (int i = 0; i < lenArr; ++i)
+    {
+        std::cout << arr[i] << " | ";
+    }
+}
+
+void Arrays (double a[16], std::string path) {
+
+    std::ifstream file;
+    file.open(path);
+
+    if (!file.is_open())
+    {
+        std::cout << "no file\n" << std::endl;
+       
+    }
+    else {
+
+        // Ввод значений в массив
+        double s = 0;
+        int i = 0;
+        for (file >> s; !file.eof(); file >> s)
+        {
+            //std::cout << s<<
+            a[i] = s;
+            i += 1;
+        }
+
+        file.close();
+    };
+}
+int main()
+{
+    std::string path[5];
+
+        // Ввод пути к файлу .txt
+        for (int i = 0; i < 5; i++)
+        {
+            std::cout << "Enter path: ";
+            std::cin >> path[i];
+        }
+
+    double a[5][16];
+
+    for (int j = 0; j < 5; j++)
+    {
+        Arrays(a[j], path[j]);
+    }
+
+    double res[16] = { 0 };
+
+    for (int i = 0; i < 16; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            res[i] += a[j][i];
+        }
+
+    }
+
+    double result = res[0];
+    int section = 0;
+
+    for (int i = 1; i < 16; i++)
+    {
+        if (result < res[i]) {
+            result = res[i];
+            section = i;
+        }
+    }
+
+    int hourLeft = 8 + section / 2;
+    int minuteLeft = 30 * (section % 2);
+
+    int hourRight = 8 + (section + 1) / 2;
+    int minuteRight = 30 * ((section + 1) % 2);
+
+    //std::cout << section << " " << section / 2 << " " << section % 2 << std::endl;
+   //std::cout << section << " " << hourLeft << " minuteRight" << minuteLeft << std::endl;
+
+    printf("%d:%02d-%d:%02d", hourLeft, minuteLeft, hourRight, minuteRight);
+
+
+
+}
+
